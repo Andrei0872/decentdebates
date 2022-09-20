50710af523d01c777d34bcd42ff94dde5eb545eb
Andrei Gatej <andreigtj01@gmail.com>
2022-09-20 23:16:21 +0300

    chore(client): add SCSS

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
