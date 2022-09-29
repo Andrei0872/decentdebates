d936859e66069f1bf175a3bc5deb8dcd4f146cd9
Andrei Gatej <andreigtj01@gmail.com>
2022-09-30 00:20:20 +0300

    feat(server): connect to the database & make pool accessible

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbModule, PG_PROVIDER_TOKEN } from './db/db.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor (@Inject(PG_PROVIDER_TOKEN) pool: Pool) {
+    pool.query('SELECT NOW()', (err, res) => {
+      console.log(err, res)
+      pool.end()
+    })
+  }
+}
---
+++ server/src/db/db.module.ts
@@ -0,0 +1,57 @@
+import { ConsoleLogger, FactoryProvider, Logger, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
+
+const LOGGER_CONTEXT = 'DB';
+
+export const PG_PROVIDER_TOKEN = 'PG_PROVIDER_TOKEN';
+
+const PG_PROVIDER: FactoryProvider<Pool> = {
+  provide: PG_PROVIDER_TOKEN,
+  useFactory: async (cs: ConfigService) => {
+    const logger = new Logger(LOGGER_CONTEXT);
+    
+    const pool = new Pool({
+      user: cs.get('POSTGRES_USER'),
+      host: cs.get('POSTGRES_HOST'),
+      database: cs.get('POSTGRES_DB'),
+      password: cs.get('POSTGRES_PASSWORD'),
+      port: cs.get('POSTGRES_PORT'),
+    });
+
+    try {
+      await new Promise((resolve, reject) => {
+        pool.query('SELECT NOW()', (err, res) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve(res);
+        });
+      })
+
+      logger.log('Successfully connected to the database.');
+    } catch (err) {
+      logger.error('An error occurred while connecting to the database', err);
+      pool.end();
+      process.exit(1);
+    }
+
+    return pool;
+  },
+  inject: [ConfigService]
+}
+
+@Module({
+  providers: [PG_PROVIDER],
+  exports: [
+    PG_PROVIDER,
+    ConfigModule,
+  ],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../.env',
+    }),
+  ]
+})
+export class DbModule {}
