c00657ff128d42b54e143a7e0e967da413968fa1
Andrei Gatej <andreigtj01@gmail.com>
2023-02-15 17:55:44 +0200

    feat: add basic styling to login

--- next/src/components/Login/Login.tsx
+++ next/src/components/Login/Login.tsx
@@ -1,9 +1,9 @@
 import { setCurrentUser } from "@/store/slices/user.slice";
 import { api } from "@/utils/api";
 import { useAppDispatch, useAppSelector } from "@/utils/hooks/store";
-import { redirect } from "next/navigation";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form"
+import styles from '@/styles/shared/form.module.scss';
 
 export interface LoginData {
   emailOrUsername: string;
@@ -29,8 +29,10 @@ function Login () {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div className={styles.container}>
+      <h2 className={styles.title}>Login</h2>
+
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <input type="text" {...register('emailOrUsername')} />
         <input type="password" {...register('password')} />
 
