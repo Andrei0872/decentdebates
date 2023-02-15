3d50d88474176e3c40bdd4a3029e7f58383c0f20
Andrei Gatej <andreigtj01@gmail.com>
2023-02-16 00:04:01 +0200

    chore(api): set up `knex` for seeds

---
+++ api/knexfile.ts
@@ -0,0 +1,19 @@
+console.log('foo');
+import * as dotenv from 'dotenv'
+
+dotenv.config({
+  path: '../.env'
+});
+
+export default {
+  client: 'pg',
+  seeds: {
+    directory: './data/seeds',
+  },
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  }
+}
