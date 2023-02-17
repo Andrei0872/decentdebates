94075aae6ccbf7a26ff234cc4f835c66b6502c06
Andrei Gatej <andreigtj01@gmail.com>
2023-02-17 23:53:08 +0200

    feat(debates): select debates along with their corresp tags

--- api/src/entities/debates/debates.service.ts
+++ api/src/entities/debates/debates.service.ts
@@ -9,19 +9,33 @@ export class DebatesService {
   
   async getAll (): Promise<Debate[]> {
     const client = await this.pool.connect();
-    const sqlStr = `
+    let sqlStr = `
+      with debates_tags as (
+        select
+          d.id "debateId",
+          string_agg(dt.name, ',') "tags"
+        from debate d
+        join assoc_debate_tag adt
+          on adt.debate_id = d.id
+        join debate_tag dt
+          on dt.id = adt.tag_id
+        group by d.id
+      )
       select
         d.id,
         d.title,
         d.created_at "createdAt",
         d.modified_at "modifiedAt",
         u.username,
-        u.id "userId"
+        u.id "userId",
+        dts."tags"
       from debate d
       join ticket t
         on d.ticket_id = t.id
       join "user" u
         on u.id = t.created_by
+      join debates_tags dts
+        on d.id = dts."debateId"
     `;
 
     try {
