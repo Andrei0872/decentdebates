09d845ddeb1ab8b08d73c0a3f77eef4395eea178
Andrei Gatej <andreigtj01@gmail.com>
2023-02-21 18:44:31 +0200

    fix(db): move everything related to `debates` in a single seed file

--- api/data/seeds/03_debates.ts
+++ api/data/seeds/03_debates.ts
@@ -1,21 +1,45 @@
 import { Knex } from "knex";
+import * as bcrypt from 'bcrypt';
 
 export async function seed(knex: Knex): Promise<void> {
     await knex("ticket").del();
     await knex("debate").del();
     await knex("assoc_debate_tag").del();
+    await knex("user").del();
+    await knex("debate_tag").del();
+
+    const [user, ...moderators] = await knex("user").insert([
+        {
+            username: "foo.bar",
+            password: await bcrypt.hash('pas123', 10),
+            email: 'foo@bar.com',
+            role: 'USER',
+        },
+        {
+            username: "mod.bar",
+            password: await bcrypt.hash('pas123', 10),
+            email: 'mod@bar.com',
+            role: 'MODERATOR',
+        },
+        {
+            username: "mod.beer",
+            password: await bcrypt.hash('pas123', 10),
+            email: 'mod@beer.com',
+            role: 'MODERATOR',
+        },
+    ], ['id']);
 
     const ticketsIds = await knex("ticket").insert([
-        { created_by: 1, assigned_to: 2, board_list: 'ACCEPTED' },
-        { created_by: 1, assigned_to: 2, board_list: 'ACCEPTED' },
-        { created_by: 1, assigned_to: 2, board_list: 'ACCEPTED' },
-        { created_by: 1, assigned_to: 2, board_list: 'ACCEPTED' },
-        { created_by: 1, assigned_to: 2, board_list: 'ACCEPTED' },
+        { created_by: user.id, assigned_to: moderators[0].id, board_list: 'ACCEPTED' },
+        { created_by: user.id, assigned_to: moderators[0].id, board_list: 'ACCEPTED' },
+        { created_by: user.id, assigned_to: moderators[0].id, board_list: 'ACCEPTED' },
+        { created_by: user.id, assigned_to: moderators[0].id, board_list: 'ACCEPTED' },
+        { created_by: user.id, assigned_to: moderators[0].id, board_list: 'ACCEPTED' },
 
-        { created_by: 1, assigned_to: null, board_list: 'PENDING' },
-        { created_by: 1, assigned_to: null, board_list: 'PENDING' },
+        { created_by: user.id, assigned_to: null, board_list: 'PENDING' },
+        { created_by: user.id, assigned_to: null, board_list: 'PENDING' },
 
-        { created_by: 1, assigned_to: 2, board_list: 'IN REVIEW' },
+        { created_by: user.id, assigned_to: moderators[0].id, board_list: 'IN REVIEW' },
     ], ['id']);
 
     const debateIds = await knex("debate").insert([
@@ -31,17 +55,23 @@ export async function seed(knex: Knex): Promise<void> {
         { ticket_id: ticketsIds[7].id, title: "in review topic#1", },
     ], ['id']);
 
+    const tags = await knex("debate_tag").insert([
+        { name: "history" },
+        { name: "community" },
+        { name: "health" }
+    ], ['id']);
+
     await knex("assoc_debate_tag").insert([
-        { debate_id: debateIds[0].id, tag_id: 1 },
-        { debate_id: debateIds[0].id, tag_id: 2 },
-        { debate_id: debateIds[1].id, tag_id: 1 },
-        { debate_id: debateIds[2].id, tag_id: 2 },
-        { debate_id: debateIds[2].id, tag_id: 3 },
-        { debate_id: debateIds[3].id, tag_id: 3 },
-        { debate_id: debateIds[4].id, tag_id: 3 },
-        { debate_id: debateIds[5].id, tag_id: 1 },
-        { debate_id: debateIds[6].id, tag_id: 2 },
-        { debate_id: debateIds[7].id, tag_id: 3 },
+        { debate_id: debateIds[0].id, tag_id: tags[0].id },
+        { debate_id: debateIds[0].id, tag_id: tags[1].id },
+        { debate_id: debateIds[1].id, tag_id: tags[0].id },
+        { debate_id: debateIds[2].id, tag_id: tags[1].id },
+        { debate_id: debateIds[2].id, tag_id: tags[2].id },
+        { debate_id: debateIds[3].id, tag_id: tags[2].id },
+        { debate_id: debateIds[4].id, tag_id: tags[2].id },
+        { debate_id: debateIds[5].id, tag_id: tags[0].id },
+        { debate_id: debateIds[6].id, tag_id: tags[1].id },
+        { debate_id: debateIds[7].id, tag_id: tags[2].id },
     ]);
 };
 
