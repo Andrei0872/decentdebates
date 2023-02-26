50495aa6393d235aab0350e5f1d16b9ad9098a47
Andrei Gatej <andreigtj01@gmail.com>
2023-02-26 08:25:55 +0200

    feat: add basic argument editor for creating an argument

---
+++ next/src/components/ArgumentEditor/ArgumentEditor.tsx
@@ -0,0 +1,40 @@
+import styles from './ArgumentEditor.module.scss';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import { editorConfig } from './config';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import ToolbarPlugin from './plugins/ToolbarPlugin';
+
+function Placeholder() {
+  return <div className={styles.placeholder}>Describe your argument here...</div>;
+}
+
+function ArgumentEditor() {
+  return (
+    <LexicalComposer initialConfig={editorConfig}>
+      <div className={styles.container}>
+        <ToolbarPlugin />
+        <div className={styles.inner}>
+          <RichTextPlugin
+            contentEditable={<ContentEditable className={styles.input} />}
+            placeholder={<Placeholder />}
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          {/* <HistoryPlugin />
+          <TreeViewPlugin />
+          <AutoFocusPlugin />
+          <CodeHighlightPlugin />
+          <ListPlugin />
+          <LinkPlugin />
+          <AutoLinkPlugin />
+          <ListMaxIndentLevelPlugin maxDepth={7} />
+          <MarkdownShortcutPlugin transformers={TRANSFORMERS} /> */}
+          {/* <LexicalTableOfContentsPlugin /> */}
+        </div>
+      </div>
+    </LexicalComposer>
+  )
+}
+
+export default ArgumentEditor
---
+++ next/src/components/ArgumentEditor/config.ts
@@ -0,0 +1,36 @@
+import { InitialConfigType } from "@lexical/react/LexicalComposer";
+import styles from './ArgumentEditor.module.scss';
+
+export const editorConfig: InitialConfigType = {
+  theme: {
+    placeholder: styles.placeholder,
+    paragraph: styles.paragraph,
+    quote: styles.quote,
+    heading: {
+      h1: styles.h1,
+      h2: styles.h2,
+      h3: styles.h3,
+      h4: styles.h4,
+      h5: styles.h5
+    },
+    list: {
+      nested: {
+        listitem: styles.nestedListItem
+      },
+      ol: styles.ol,
+      ul: styles.ul,
+      listitem: styles.listItem
+    },
+    link: styles.link,
+    text: {
+      bold: styles.textBold,
+      italic: styles.textItalic,
+      underline: styles.textUnderline,
+      strikethrough: styles.textStrikethrough,
+    },
+  },
+  onError(err) {
+    throw err;
+  },
+  namespace: 'decentdebates'
+}
