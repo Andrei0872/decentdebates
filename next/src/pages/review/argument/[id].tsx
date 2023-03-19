4081829b8deceb9d4d51edcf81121f17482b6903
Andrei Gatej <andreigtj01@gmail.com>
2023-03-19 17:08:03 +0200

    feat(moderator): add basic argument review page layout

--- next/src/pages/review/argument/[id].tsx
+++ next/src/pages/review/argument/[id].tsx
@@ -1,17 +1,54 @@
+import CommentsLayout from '@/components/Comments/CommentsLayout';
+import Layout from '@/components/Layout/Layout';
 import { selectPreviewedCard } from '@/store/slices/moderator.slice';
 import { setCurrentUser } from '@/store/slices/user.slice';
 import { useAppDispatch, useAppSelector } from '@/utils/hooks/store';
 import { useRouter } from 'next/router'
 import { useEffect } from 'react';
 
-function Debate() {
+import styles from '@/styles/ReviewArgument.module.scss';
+import Comment from '@/components/Comments/Comment';
+import { Callout } from '@blueprintjs/core';
+
+const comments = [...new Array(3)];
+
+function ArgumentContent() {
+  return (
+    <div>
+      <Callout className={styles.debateInfo}>
+        <div className={styles.debateTitleContainer}>
+          <i className={styles.debateIcon}></i>
+          <h3>Debate title</h3>
+        </div>
+      </Callout>
+
+      <div className={styles.argContainer}>
+        <h2 className={styles.argTitle}>Arg title</h2>
+
+        <div className={styles.argInfo}>
+          <div className={styles.argType}>CON</div>
+
+          <div>
+            Counterargument to <span>Counterargument title</span>
+          </div>
+        </div>
+
+        <div className={styles.argContent}>
+          arg content
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Argument() {
   const router = useRouter()
   const { id } = router.query
 
-  // console.log(id, router.isReady);
-
   const previewedCard = useAppSelector(selectPreviewedCard);
   const dispatch = useAppDispatch();
+
+  console.log(previewedCard);
 
   useEffect(() => {
     if (!previewedCard) {
@@ -20,9 +57,26 @@ function Debate() {
     }
   }, []);
 
+  const redirectBack = () => {
+    router.back();
+  }
+
+
   return (
-    <div>a</div>
+    <Layout>
+      <section className={styles.buttons}>
+        <button onClick={redirectBack} type='button'>Back</button>
+      </section>
+
+      <CommentsLayout mainContent={<ArgumentContent />}>
+        {
+          comments.map(c => (
+            <Comment />
+          ))
+        }
+      </CommentsLayout>
+    </Layout>
   )
 }
 
-export default Debate;
+export default Argument;
