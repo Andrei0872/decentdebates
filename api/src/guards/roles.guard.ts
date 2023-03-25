5b644afa81eec5afa87095a2d1e59381b858c7d0
Andrei Gatej <andreigtj01@gmail.com>
2023-03-25 15:57:09 +0200

    fix(RolesGuard): take into account request handlers

--- api/src/guards/roles.guard.ts
+++ api/src/guards/roles.guard.ts
@@ -7,18 +7,21 @@ import { UserCookieData, UserRoles } from 'src/entities/user/user.model';
 export class RolesGuard implements CanActivate {
   constructor (private reflector: Reflector) { }
   
+  // TODO: should take into account decorators placed on both the class & handler?
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const roles = this.reflector.get<UserRoles[]>('roles', context.getClass());
-    if (!roles) {
+    const classRoles = this.reflector.get<UserRoles[]>('roles', context.getClass());
+    const handlerRoles = this.reflector.get<UserRoles[]>('roles', context.getHandler());
+    const hasRoles = classRoles || handlerRoles;
+    if (!hasRoles) {
       return true;
     }
 
     const request = context.switchToHttp().getRequest();
     const user = request.session.user as UserCookieData;
 
-    return this.doRolesMatch(roles, user.role);
+    return this.doRolesMatch(classRoles || handlerRoles, user.role);
   }
 
   private doRolesMatch (acceptedRoles: UserRoles[], currentUserRole: UserRoles): boolean {
