13ef35d002d17bb5ca0a19675544334fc45c1ed9
Andrei Gatej <andreigtj01@gmail.com>
2023-03-30 22:47:54 +0300

    feat: add `CardSlider` component

---
+++ next/src/components/CardSlider/Card/Card.tsx
@@ -0,0 +1,8 @@
+
+function Card() {
+  return (
+    <div>Card</div>
+  )
+}
+
+export default Card
---
+++ next/src/components/CardSlider/CardSlider.tsx
@@ -0,0 +1,57 @@
+import { Icon } from '@blueprintjs/core'
+import React, { ReactNode, useEffect, useMemo, useRef, useState } from 'react'
+import styles from './CardSlider.module.scss'
+
+interface Props {
+  children: ReactNode
+}
+
+function CardSlider(props: Props) {
+  const [sliderIdx, setSliderIdx] = useState(0);
+
+  const sliderContainerRef = useRef<HTMLDivElement | null>(null);
+  const cardsContainerRef = useRef<HTMLUListElement | null>(null);
+
+  // Smaller window scroll.
+  // const slidesCount = sliderContainerRef.current ? Math.floor((cardsContainerRef.current!.scrollWidth) / (sliderContainerRef.current.offsetWidth - 400)) : 1;
+  // const scrollAmount = sliderContainerRef.current ? sliderContainerRef.current.offsetWidth - 400 : 0;
+
+  const slidesCount = sliderContainerRef.current ? Math.ceil((cardsContainerRef.current!.scrollWidth) / sliderContainerRef.current.clientWidth) : 1;
+  const scrollAmount = sliderContainerRef.current ? sliderContainerRef.current.offsetWidth: 0;
+
+  const goNextSlide = () => {
+    setSliderIdx(sliderIdx + 1);
+    cardsContainerRef.current!.scrollLeft += scrollAmount;
+  }
+
+  const goPrevSlide = () => {
+    setSliderIdx(sliderIdx - 1);
+    cardsContainerRef.current!.scrollLeft -= scrollAmount;
+  }
+
+  const canSlideNext = sliderIdx < slidesCount - 1;
+  const canSlidePrev = sliderIdx >= 1;
+
+  return (
+    <div ref={sliderContainerRef} className={styles.container}>
+      <ul ref={cardsContainerRef} className={styles.cards}>
+        {React.Children.map(props.children, elem => {
+          return (
+            <li className={styles.card}>
+              {elem}
+            </li>
+          )
+        })}
+      </ul>
+
+      <button disabled={!canSlidePrev} onClick={goPrevSlide} type='button'>
+        <Icon icon="chevron-left" />
+      </button>
+      <button disabled={!canSlideNext} onClick={goNextSlide} type='button'>
+        <Icon icon="chevron-right" />
+      </button>
+    </div>
+  )
+}
+
+export default CardSlider
