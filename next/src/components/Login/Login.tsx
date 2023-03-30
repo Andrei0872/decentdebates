f36465fcaaabacfde809c419a0b34c78f2f86da2
Andrei Gatej <andreigtj01@gmail.com>
2023-03-30 17:21:14 +0300

    fix(Login): prevent visiting page if user logged in

--- next/src/components/Login/Login.tsx
+++ next/src/components/Login/Login.tsx
@@ -1,9 +1,10 @@
-import { setCurrentUser } from "@/store/slices/user.slice";
+import { selectCurrentUser, setCurrentUser, User, UserRoles } from "@/store/slices/user.slice";
 import { api } from "@/utils/api";
 import { useAppDispatch, useAppSelector } from "@/utils/hooks/store";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form"
 import styles from '@/styles/shared/form.module.scss';
+import { useEffect } from "react";
 
 export interface LoginData {
   emailOrUsername: string;
@@ -17,14 +18,37 @@ function Login () {
   } = useForm<LoginData>();
 
   const dispatch = useAppDispatch();
+  const user = useAppSelector(selectCurrentUser);
+
   const router = useRouter();
+
+  useEffect(() => {
+    if (!!user) {
+      router.back();
+      return;
+    }
+  }, []);
+  
 
   const onSubmit = (data: LoginData) => {
     api.post('auth/login', data)
       .then(r => r.data.data)
-      .then(user => {
+      .then((user: User) => {
         dispatch(setCurrentUser(user));
-        router.push('/debates');
+
+        switch (user?.role) {
+          case UserRoles.USER: {
+            router.push('/debates');
+            return;
+          }
+          case UserRoles.MODERATOR: {
+            router.push('/activity');
+            return;
+          }
+          case UserRoles.ADMIN: {
+            return;
+          }
+        }
       });
   }
 
