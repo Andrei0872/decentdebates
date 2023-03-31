a955bcba34c3e2f1d3299674db5a6183d5eac787
Andrei Gatej <andreigtj01@gmail.com>
2023-03-31 18:52:01 +0300

    refactor(Input): integrate with `react-hook-form`'s `register`

--- next/src/components/Input/Input.tsx
+++ next/src/components/Input/Input.tsx
@@ -2,24 +2,33 @@ import { debounce } from '@/utils/debounce';
 import { useCallback } from 'react';
 import styles from './Input.module.scss';
 import { InputHTMLAttributes } from 'react'
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 interface Props {
   debounceMs?: number;
   inputProps?: InputHTMLAttributes<any>;
-  onChange: (v: string) => void;
+  register?: UseFormRegisterReturn;
+  onChange?: (v: string) => void;
 }
 
 function Input(props: Props) {
   const onChangeDebounced = useCallback(
     debounce((ev: any) => {
       const value = ev.target.value;
-      props.onChange(value);
+      props.onChange?.(value);
     }, 500),
     [],
   );
 
   return (
-    <input className={styles.input} type="text" placeholder='Search...' {...props.inputProps} onChange={onChangeDebounced} />
+    <input
+      className={styles.input}
+      type="text"
+      placeholder='Search...'
+      {...props.inputProps}
+      {...props.register}
+      onChange={onChangeDebounced}
+    />
   )
 }
 
