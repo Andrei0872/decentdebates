db478caaf797f27236cc189a516ee3cd2bb770d3
Andrei Gatej <andreigtj01@gmail.com>
2023-04-01 23:22:19 +0300

    feat(about): style page

--- next/src/pages/about.tsx
+++ next/src/pages/about.tsx
@@ -1,8 +1,20 @@
+import Layout from '@/components/Layout/Layout'
 import React from 'react'
+import styles from '@/styles/About.module.scss'
 
 function About() {
   return (
-    <div>about</div>
+    <Layout>
+      <div className={styles.container}>
+        <section className={styles.body}>
+          <h1 className={styles.title}>About</h1>
+
+          <p className={styles.content}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est beatae necessitatibus dolores itaque blanditiis, ipsum voluptate corporis odio mollitia quod perferendis maiores culpa totam sunt officiis quidem magnam ab temporibus?
+          </p>
+        </section>
+      </div>
+    </Layout>
   )
 }
 
