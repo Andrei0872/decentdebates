e551e29a6fcf5a397e0f29bf4a0fe1884792c70c
Andrei Gatej <andreigtj01@gmail.com>
2023-04-01 14:52:48 +0300

    feat: style _Login_ page

--- next/src/components/Login/Login.tsx
+++ next/src/components/Login/Login.tsx
@@ -3,15 +3,18 @@ import { api } from "@/utils/api";
 import { useAppDispatch, useAppSelector } from "@/utils/hooks/store";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form"
-import styles from '@/styles/shared/form.module.scss';
+import formStyles from '@/styles/shared/form.module.scss';
 import { useEffect } from "react";
+import Input from "../Input/Input";
+import buttonStyles from '@/styles/shared/button.module.scss'
+import styles from './Login.module.scss';
 
 export interface LoginData {
   emailOrUsername: string;
   password: string;
 }
 
-function Login () {
+function Login() {
   const {
     register,
     handleSubmit,
@@ -28,7 +31,7 @@ function Login () {
       return;
     }
   }, []);
-  
+
 
   const onSubmit = (data: LoginData) => {
     api.post('auth/login', data)
@@ -53,14 +56,19 @@ function Login () {
   }
 
   return (
-    <div className={styles.container}>
+    <div className={formStyles.container}>
       <h2 className={styles.title}>Login</h2>
 
-      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" {...register('emailOrUsername')} />
-        <input type="password" {...register('password')} />
+      <form className={formStyles.form} onSubmit={handleSubmit(onSubmit)}>
+        <Input inputProps={{ placeholder: 'Email or username' }} register={register('emailOrUsername')} />
+        <Input inputProps={{ type: 'password', placeholder: 'Password' }} register={register('password')} />
 
-        <button type="submit">Submit</button>
+        <button
+          className={`${buttonStyles.button} ${buttonStyles.primary} ${buttonStyles.outlined}`}
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </div>
   )
