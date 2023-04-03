84fed162e3d6751ae99286e1ce2a83ace9bb782c
Andrei Gatej <andreigtj01@gmail.com>
2023-04-03 13:33:15 +0300

    feat(Tags): allow creating new tags

--- next/src/components/Tags/Tags.tsx
+++ next/src/components/Tags/Tags.tsx
@@ -6,12 +6,16 @@ import { ReactNode, useMemo, useState } from 'react';
 
 interface Props {
   debateTags: Tag[];
+  canCreateTags?: boolean;
 }
 
 function Tags(props: Props) {
-  const { debateTags } = props;
+  const { debateTags: tags } = props;
+  const canCreateTags = props.canCreateTags === true;
 
   const [selectedTagsIds, setSelectedTagsIds] = useState<number[]>([]);
+  const [debateTags, setDebateTags] = useState(tags);
+  const [createdTagsIds, setCreatedTagsIds] = useState<{ [k: number]: boolean }>({});
 
   const itemRenderer: ItemRenderer<Tag> = (tag, props) => {
     if (!props.modifiers.matchesPredicate) {
@@ -28,11 +32,33 @@ function Tags(props: Props) {
         elementRef={props.ref}
         roleStructure="listoption"
         shouldDismissPopover={false}
+        className={`${createdTagsIds[tag.id] ? styles.createdItem : ''}`}
       />
     )
   }
 
   const handleItemSelect = (tag: Tag) => {
+    setDebateTags(debateTags => {
+      const doesTagExist = !!debateTags.find(dt => dt.id === tag.id);
+      if (doesTagExist) {
+        return debateTags;
+      }
+
+      return [...debateTags, tag];
+    });
+
+    setCreatedTagsIds(createdTagsIds => {
+      const doesTagExist = !!debateTags.find(dt => dt.id === tag.id);
+      if (doesTagExist) {
+        return createdTagsIds;
+      }
+
+      return {
+        ...createdTagsIds,
+        [tag.id]: true,
+      }
+    });
+
     setSelectedTagsIds((selectedTags) => {
       const isItemSelected = !!selectedTags.find(tId => tId === tag.id);
       if (isItemSelected) {
@@ -72,6 +98,30 @@ function Tags(props: Props) {
     return tag1.name.toLowerCase() === tag2.name.toLowerCase();
   }
 
+  const createTag = (query: string): Tag => {
+    return {
+      id: Date.now(),
+      name: query,
+    };
+  }
+
+  const renderCreatedTag = (
+    query: string,
+    active: boolean,
+    handleClick: React.MouseEventHandler<HTMLElement>
+  ) => {
+    return (
+      <MenuItem2
+        icon="add"
+        text={`Create "${query}"`}
+        roleStructure="listoption"
+        active={active}
+        onClick={handleClick}
+        shouldDismissPopover={false}
+      />
+    );
+  }
+
   const selectedTags = useMemo(() => {
     return selectedTagsIds.map(tId => debateTags.find(dt => dt.id === tId)!);
   }, [selectedTagsIds]);
@@ -86,11 +136,21 @@ function Tags(props: Props) {
         tagRenderer={tagRenderer}
         tagInputProps={{
           onRemove: handleTagRemove,
+          tagProps: (_tagElement, index) => {
+            const tag = selectedTags[index];
+            const isTagNew = !!createdTagsIds[tag.id];
+
+            return {
+              className: `${styles.tag} ${isTagNew ? styles.tagNew : ''}`
+            }
+          },
         }}
         itemPredicate={filterTag}
         resetOnSelect={true}
         itemsEqual={areTagsEqual}
         noResults={<MenuItem2 disabled={true} text="No results." roleStructure="listoption" />}
+        createNewItemFromQuery={canCreateTags ? createTag : undefined}
+        createNewItemRenderer={canCreateTags ? renderCreatedTag : undefined}
       />
     </div>
   )
